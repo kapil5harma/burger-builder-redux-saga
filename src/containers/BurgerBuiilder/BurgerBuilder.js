54f316b4078812c3d48614d5f6ef9cb97ce5f8fe
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-27 16:50:17 +0530

    Burger Component created and used in BurgerBuilder Component

--- src/containers/BurgerBuiilder/BurgerBuilder.js
+++ src/containers/BurgerBuiilder/BurgerBuilder.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // The method to initialise state before React 16, also valid now, but we will use a newwer method.
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
