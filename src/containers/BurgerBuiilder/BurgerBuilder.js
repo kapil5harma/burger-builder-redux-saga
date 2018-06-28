4c8a36a2720c3a69070506714c6871c880cb454e
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-28 14:20:18 +0530

    Show Modal only on clicking Order Now button

--- src/containers/BurgerBuiilder/BurgerBuilder.js
+++ src/containers/BurgerBuiilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 12.5,
@@ -26,7 +28,12 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 100,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
+  };
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
   };
 
   updatePurchaseState = ingredients => {
@@ -83,6 +90,9 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal show={this.state.purchasing}>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
@@ -90,6 +100,7 @@ class BurgerBuilder extends Component {
           disabled={disabledInfo}
           price={this.state.totalPrice}
           purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
         />
       </Aux>
     );
