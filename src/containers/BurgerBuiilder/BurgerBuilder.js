c2c0cb31b4867a21085f03af7037f0e7b3553381
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-28 14:33:43 +0530

    Created Cancel Order handler to hide Modal on clicking on Backdrop

--- src/containers/BurgerBuiilder/BurgerBuilder.js
+++ src/containers/BurgerBuiilder/BurgerBuilder.js
@@ -36,6 +36,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true });
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
   updatePurchaseState = ingredients => {
     const sum = Object.keys(ingredients)
       .map(igKey => {
@@ -90,7 +94,10 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
