a112f3b3908d97daf72fcf1cf4f5ea6b913c3058
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-12 15:24:10 +0530

    Adding an Orders Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuiilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   // Lines added to just test that unmount gets called once BurgerBuilder is hidden.
@@ -25,10 +26,11 @@ class App extends Component {
           {this.state.show ? <BurgerBuilder /> : null} */}
 
           {/* <BurgerBuilder /> */}
-          <Route path="/" exact component={BurgerBuilder} />
           {/* <Checkout /> */}
           {/* <Route path="/checkout" exact component={Checkout} /> */}
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
         </Layout>
       </div>
     );
