2b8f0709831bcc0597275a0560bb810d7400842d
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-12 14:04:41 +0530

    Navigating to the ContactData component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,10 +19,30 @@ class ContactData extends Component {
       <div className={classes.ContactData}>
         <h4>Enter your Contact data:</h4>
         <form>
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="email" name="email" placeholder="Your Email" />
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postal" placeholder="Postal Code" />
+          <input
+            className={classes.Input}
+            type="text"
+            name="name"
+            placeholder="Your Name"
+          />
+          <input
+            className={classes.Input}
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="street"
+            placeholder="Street"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="postal"
+            placeholder="Postal Code"
+          />
           <Button btnType="Success">ORDER</Button>
         </form>
       </div>
